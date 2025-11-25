de670ce42d8135d88b1eb14b222da0021da652a8
TERA <p.tyche92@gmail.com>
2025-11-25 18:07:55 +0900

    최종 취합페이지 추가

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+pages = {
+    "최종 모델 리포트": [
+        st.Page("ne_eun/view.py", title="최종모델", url_path="final_model"),
+    ],
+    "비교 모델 리포트": [
+        st.Page("ne_eun/view.py", title="박내은", url_path="neeun"),
+        st.Page("grkim/app.py", title="김가람", url_path="garam"),
+    ],
+}
+pg = st.navigation(pages)
+pg.run()
