e32bca8ef5178c89cfcfd709c204290b8538b02e
TERA <p.tyche92@gmail.com>
2025-11-26 09:13:12 +0900

    [feat] 덕규,현욱님 페이지 추가

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ pages = {
         st.Page("ne_eun/view.py", title="박내은", url_path="neeun"),
         st.Page("grkim/app.py", title="김가람", url_path="garam"),
         st.Page("yiseon/yiseon.py", title="장이선", url_path="yiseon"),
+        st.Page("dkjung/streamlit/mainboard.py", title="정덕규", url_path="dkjung"),
+        st.Page("hyeun_uk/streamlit.py", title="강현욱", url_path="hyeun_uk"),
     ],
 }
 pg = st.navigation(pages)
